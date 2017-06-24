03d5da779878b655cdcb7a1f590aab733cb0354b
Desireé Bryant <desireebryant@users.noreply.github.com>
2017-06-23 21:23:50 -0500

    Update db_connect.php

--- _original/projects/leanmagazine/includes/db_connect.php
+++ _original/projects/leanmagazine/includes/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 	//Database login information
-	$host="localhost";
-	$user="dbryant_bunny";
-	$pass="x2c3h4j6";
-	$db="dbryant_leanmagazine";
+	$host="******";
+	$user="******";
+	$pass="******";
+	$db="******";
 ?>
