fad63d3b03f264a57be87752fab29b5abf02d4d8
Desireé Bryant <desireebryant@users.noreply.github.com>
2017-06-23 21:23:14 -0500

    Update db_connect.php

--- _original/projects/horoscope/includes/db_connect.php
+++ _original/projects/horoscope/includes/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 	//Database login information
-	$host="localhost";
-	$user="dbryant_bunny";
-	$pass="x2c3h4j6";
-	$db="dbryant_horoscope";
+	$host="******";
+	$user="******";
+	$pass="******";
+	$db="********";
 ?>
